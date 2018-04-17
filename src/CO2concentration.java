7cfea545400e75f0e0bef0898556b114f021623f
Lyudmyla25 <37074842+Lyudmyla25@users.noreply.github.com>
2018-04-17 16:39:30 +0200

    Delete CO2concentration.java

--- src/CO2concentration.java
+++
@@ -1,8 +0,0 @@
-public class CO2concentration {
-
-    final static double low_normal_concentration = 250;
-    final static double hight_normal_concentration = 1500;
-
-    static void check_air(int id_room) {
-    }
-}
