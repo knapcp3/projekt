008e6d629f181571a36b5458f97c4edb04572c4b
Jakub Knapik <jakubknapik3@gmail.com>
2018-04-17 16:54:09 +0200

    add building

--- src/Space.java
+++ src/Space.java
@@ -1,9 +1,12 @@
+import java.util.Map;
+
 interface Space {
 
     int getId();
     int getCurTemperature();
     int getCurCO2Concentration();
     Boolean getLightState(int lightNr);
+    Map getLightStates();
 
     void setTemperature(int temperature);
     void setCO2Concentration(int CO2Concentration);
