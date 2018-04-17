957a02796048a4b6359e1c9ce01858d3afe5d678
Lyudmyla <ludochka.zahar@gmail.com>
2018-04-17 17:16:20 +0200

    com

---
+++ src/CO2Concentration.java
@@ -0,0 +1,7 @@
+public class CO2Concentration {
+    final static double low_normal_concentration = 250;
+    final static double hight_normal_concentration = 1500;
+
+    static void check_air(int id_room) {
+    }
+}
---
+++ src/Light.java
@@ -0,0 +1,10 @@
+public class Light {
+    static void light_on(int id_room) {
+    }
+
+    static void light_off(int id_room) {
+    }
+
+    static void all_light_off() {
+    }
+}
