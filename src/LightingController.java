dc89678c3f372bfa72dbf42bad37f0e3519c4aa0
knapcp3 <jakubknapik3@gmail.com>
2018-04-17 16:56:26 +0200

    Delete LightingController.java

--- src/LightingController.java
+++
@@ -1,8 +0,0 @@
-class LightingController {
-
-    static void switchLight(Room room, int lightNr){
-        boolean newState = !room.getLightState(lightNr);
-        room.setLight(lightNr, newState);
-    }
-
-}
