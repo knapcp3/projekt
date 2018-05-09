a0717337e8902a9e493c51ea0faca7f4d3ea2b9e
Jakub Knapik <jakubknapik3@gmail.com>
2018-05-10 00:39:05 +0200

    correct main

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main{
     public static void main(String[] args){
         Room room1 = new Room();
         Room room2 = new Room();
-        AirConditioning.changeTemperature(room1, 15);
-        AirConditioning.changeTemperature(room2, 40);
+        room1.setTemperature(15);
+        room2.setTemperature(40);
 
         Room[] room;
         room = new Room [4];
         for (int i=0;i<4;i++)
             room[i]=new Room();
-    ManagementGui inter= new ManagementGui(room);
+        ManagementGui inter= new ManagementGui(room);
         inter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         inter.setVisible(true);
 
